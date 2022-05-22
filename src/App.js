bb704296df2c3319247385c7d9a819373f2fabd3
Hunter Wallace <32723509+Hjwallace@users.noreply.github.com>
2022-05-22 12:40:43 -0600

    Saving Progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://digitalcorps.s3.amazonaws.com/students.json')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          items: json.students,
+        })
+      });
+  }
+
+
+
+  render() 
+  {
+
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } 
+    else {
+      return (
+        <div className="App">
+          <header className="App-header">
+
+
+            <div className="container">
+
+            {items.map(item => (
+
+              <div key={item.id} className="flex-item">   
+
+                <img alt='Photo Not Available' src={item.image} style={{width: 200, height: 200}} className="photoBorder"/>
+                <h5 style={{margin: 0, padding:0}}>{item.name}</h5>
+                <p style={{textAlign: 'center',margin: 0, padding:0}}>{item.team}</p>
+              </div>
+
+              ))}
+
+            </div>
+             
+            <p>Data Has Been Loaded</p>
+          </header>
+        </div>
+      );
+    }
+
+    
+  }
+
+  }
 
 export default App;
